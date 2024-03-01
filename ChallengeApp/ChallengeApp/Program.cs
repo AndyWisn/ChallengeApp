7c0efec1bf4fb11835bd720d08c953014bc5eb17
AndyWisn <andrzwisniewski@gmail.com>
2024-03-01 08:47:31 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = 'K';      // Płeć: K/k - kobieta,  M/m mężczyzna,  I/i - inna
+byte age = 29;
+
+// Zakładam, że Ewa to zawsze kobieta, inaczej warunki się nieco skomplikują
+
+if (sex == 'K' || sex == 'k')
+{
+    if (age < 30) Console.WriteLine("Kobieta poniżej 30 lat");
+    else if (age == 30 && name == "Ewa") Console.WriteLine("Ewa, lat 30");
+}
+else if ((sex == 'M' ||sex == 'm') && age < 18) Console.WriteLine("Niepłnoletni mężczyzna");
+
