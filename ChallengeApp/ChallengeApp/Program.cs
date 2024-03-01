1b668b5145a645c7198095661afbbfd062245dbb
AndyWisn <andrzwisniewski@gmail.com>
2024-03-01 10:13:00 +0100

    oprawka zad4 (dodano nawiasy poprawiono czytelność)

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,13 +1,23 @@
 ﻿var name = "Ewa";
 var sex = 'K';      // Płeć: K/k - kobieta,  M/m mężczyzna,  I/i - inna
-byte age = 29;
+byte age = 30;
 
 // Zakładam, że Ewa to zawsze kobieta, inaczej warunki się nieco skomplikują
 
-if (sex == 'K' || sex == 'k')
+if ((sex == 'K') || (sex == 'k'))
 {
-    if (age < 30) Console.WriteLine("Kobieta poniżej 30 lat");
-    else if (age == 30 && name == "Ewa") Console.WriteLine("Ewa, lat 30");
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age == 30 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 30");
+    }
 }
-else if ((sex == 'M' || sex == 'm') && age < 18) Console.WriteLine("Niepłnoletni mężczyzna");
+else if ((sex == 'M' || sex == 'm') && (age < 18))
+{
+    Console.WriteLine("Niepłnoletni mężczyzna");
+}
+
 
