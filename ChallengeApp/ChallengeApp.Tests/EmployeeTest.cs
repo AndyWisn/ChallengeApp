a40af7489248a175d7a8833437bd5d0b73be70e2
AndyWisn <andrzwisniewski@gmail.com>
2024-03-06 21:35:30 +0100

    Zadanie dzien 8 - odstępy

--- ChallengeApp/ChallengeApp.Tests/EmployeeTest.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTest.cs
@@ -4,18 +4,15 @@ namespace ChallengeApp.Tests
 {
     public class Tests
     {
-
         [Test]
         public void WhenEmployeeCollectScoresShouldReturnCorrectSum()
         {
-
             //arrange
             var Employee = new Employee("Andrzej", "Wiœniewski", 46);
             Employee.AddScore(5);
             Employee.SubScore(5);
             Employee.AddScore(6);
             Employee.SubScore(6);
-
 
             // act
             int result = Employee.Result;
@@ -24,7 +21,5 @@ namespace ChallengeApp.Tests
             Assert.AreEqual(0, result);
 
         }
-
-
     }
 }
--- ChallengeApp/ChallengeApp.Tests/TypeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -4,61 +4,59 @@ namespace ChallengeApp.Tests
     public class TypeTests
     {
         [Test]
-        public void GetEmployeeShouldReturnDifferentObjects()                       //objects
+        public void GetEmployeeShouldReturnDifferentObjects()                //objects
         {
-
             //arrange
-
             var emp1 = GetEmployee("Ala");
             var emp2 = GetEmployee("Ala");
 
+            //act
 
-            // act
-
-
-            // assert
+            //assert
             Assert.AreNotEqual(emp1, emp2);
-
         }
 
         [Test]
         public void ShouldPassWhenTablesAreSame()                           //2D tables
         {
-
             //arrange
-
             int[,] array1 = { { 1, 1 }, { 1, 1 } };
             int[,] array2 = { { 1, 1 }, { 1, 1 } };
 
-            // act
+            //act
 
-
-            // assert
+            //assert
             Assert.AreEqual(array1, array2);
+         }
 
+        [Test]
+        public void ShouldPassWhenCharTableIsSameAsString()                           //1D char table vs string
+        {
+            //arrange
+            char[] array1 = {'a','b','c'};
+            string string1 = "abc";
+
+            //act
+
+            //assert
+            Assert.AreEqual(array1, string1);
         }
 
         [Test]
         public void ShouldPassWhenEmployeesNamesAreTheSame()                //strings
         {
-
             //arrange 
-
             var empl1 = GetEmployee("Andrzej");
             var empl2 = GetEmployee("Andrzej");
 
-
             // assert
             Assert.AreEqual(empl1.name, empl2.name);
-
         }
 
         [Test]
-        public void ShouldPassWhenEmployeesAgeIsEqual()                //bytes
+        public void ShouldPassWhenEmployeesAgeIsEqual()                     //bytes
         {
-
             //arrange 
-
             var employ1 = new Employee("Andrzej", "Wiśniewski", 46);
             var employ2 = new Employee("Bożena", "Wiśniewska", 46);
 
@@ -66,15 +64,10 @@ namespace ChallengeApp.Tests
 
             // assert
             Assert.AreEqual(employ1.age, employ2.age);
-
         }
-
-
         private Employee GetEmployee(string name)
         {
             return new Employee(name);
         }
-
-
     }
 }
